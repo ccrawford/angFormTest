a1ca346d88c6aaf8f0089207378e16ed815cf184
Chris Crawford <chris@ccrawford.org>
2024-03-29 16:38:45 -0500

    Added styling. Working.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,21 @@ import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { PreferencesComponent } from './preferences/preferences.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, PreferencesComponent, HttpClientModule],
+  imports: [RouterOutlet, FormsModule, PreferencesComponent, HttpClientModule,
+    MatSliderModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/preferences/preferences.component.ts
+++ src/app/preferences/preferences.component.ts
@@ -2,16 +2,23 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-preferences',
   standalone: true,
-  imports: [FormsModule, HttpClientModule],
+  imports: [FormsModule, HttpClientModule, MatSliderModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule],
   templateUrl: './preferences.component.html',
   styleUrl: './preferences.component.css'
 })
 export class PreferencesComponent {
-  preferences: any  = {};
+  preferences: any  = { };
   scoreboxIP: string | null = null;
 
   constructor(private http: HttpClient, private route: ActivatedRoute) { }
